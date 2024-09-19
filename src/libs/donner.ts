5c78072d5bc3996e1083c2642f8e51802c96b8a0
Bacati <lucaslievre2001@gmail.com>
2024-09-19 20:54:43 +0200

    Feat: toute les configs sur la meme page plus petite modif

--- src/libs/donner.ts
+++ src/libs/donner.ts
@@ -1,8 +1,19 @@
 export const config = [
 	{
 		allMk: "560",
+		allTop:"682",
+		allAirsal:"596",
 	}
 ];
+export const general500 =[
+	{
+		roulement:"14",
+		embrayage:"44",
+		pipe:"14",
+		carbu:"105",
+		pot:"212",
+	}
+]
 export const all50Mk =[
 	{
 		cylindre:"230",
@@ -10,4 +21,15 @@ export const all50Mk =[
 		pot:"212",
 	}
 ]
- 
+export const all70top =[
+	{
+		cylindre:"188",
+		vilo:"105",
+	}
+]
+export const all70airsal =[
+	{
+		cylindre:"131",
+		vilo:"77",
+	}
+]
