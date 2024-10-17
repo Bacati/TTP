0f0b05dc71a1101a4c61382093e7ee20e25325a4
Bacati <lucaslievre2001@gmail.com>
2024-10-17 12:06:58 +0200

    changement image

--- src/libs/aLaUne.ts
+++ src/libs/aLaUne.ts
@@ -7,14 +7,15 @@ export const generateProductData = (config) => [
     title: "50cc Metrakit Alu",
     logo: "mk.svg",
     description: "Configuration AllDays",
-    typeMotor: "Derbi",
+    typeMotor1: "Derbi",
+    typeMotor: "AM6",
     price: config[0].allMk || 'N/A',
     imageSrc: `${imagePrefix}/50mk.png`,
     link: `${linkPrefix}50mk`
   },
   {
     title: "70cc Top Performance",
-    logo: "topPerf.png",
+    logo: "top.webp",
     description: "Configuration AllDays",
     typeMotor: "AM6",
     price: config[0].allTop || 'N/A',
@@ -34,13 +35,14 @@ export const generateProductData = (config) => [
     logo: "airsal.png",
     description: "Configuration AllDays",
     typeMotor: "AM6",
+    typeMotor1: "Derbi",
     price: config[0].allAirsalAlu || 'N/A',
     imageSrc: `${imagePrefix}/70airsal.png`,
     link: `${linkPrefix}70airsalAlu`
   },
   {
     title: "75cc Top performance",
-    logo: "topPerf.png",
+    logo: "top.webp",
     description: "Configuration AllDays",
     typeMotor: "AM6",
     price: config[0].allTopRose || 'N/A',
@@ -61,13 +63,14 @@ export const generateProductData = (config) => [
     logo: "brk.png",
     description: "Configuration AllDays",
     typeMotor: "AM6",
+    typeMotor1: "Derbi",
     price: config[0].all78brk || 'N/A',
     imageSrc: `${imagePrefix}/80brk.png`,
     link: `${linkPrefix}78brk`
   },
   {
     title: "86cc Top performance",
-    logo: "topPerf.png",
+    logo: "top.webp",
     description: "Configuration AllDays",
     typeMotor: "AM6",
     price: config[0].allTopalu || 'N/A',
@@ -79,6 +82,7 @@ export const generateProductData = (config) => [
     logo: "most.png",
     description: "Configuration AllDays",
     typeMotor: "AM6",
+    typeMotor1: "Derbi",
     price: config[0].all78Most || 'N/A',
     imageSrc: `${imagePrefix}/80most.png`,
     link: `${linkPrefix}78most`
