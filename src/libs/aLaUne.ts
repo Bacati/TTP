7877f0cc9aa2930b81976ef03a2235a13ca45cae
Bacati <lucaslievre2001@gmail.com>
2024-10-29 13:57:21 +0100

    moins de duplication de code

--- src/libs/aLaUne.ts
+++ src/libs/aLaUne.ts
@@ -1,4 +1,5 @@
 const linkPrefix = "/product/config?config=";
+const linkPrefixCompet = "/product/config?configCompetition=";
 const imagePrefix = "/images";
 
 // Fonction pour générer des données produits à partir de `config`
@@ -15,15 +16,16 @@ export const generateProductData = (config) => [
     link: `${linkPrefix}50mk`
   },
   {
-    title: "70cc Top Performance",
-    logo: "top.webp",
-    alt: "70 top noir",
-    description: "Configuration AllDays",
-    typeMotor: "AM6",
-    price: config[0].allTop || 'N/A',
-    imageSrc: `${imagePrefix}/topnoir.png`,
-    link: `${linkPrefix}70top`
-  },
+		title: "70cc 2Fast",
+		logo: "fast.png",
+		alt: "70 2Fast",
+		description: "Compétition",
+		typeMotor1: "Derbi",
+		typeMotor: "AM6",
+		price: config[0]?.compet70Fast || 'N/A',
+		imageSrc: `${imagePrefix}/fastCyl.png`,
+		link: `${linkPrefixCompet}70fast`
+	},
   {
     title: "Nettoyant pot",
     logo: "logo.svg",
